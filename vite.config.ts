ed1f7aa859701b75d38c4f7153a9a32763465ad1
André David dos Santos <aetdesenvolvimentoweb@gmail.com>
2026-04-21 20:02:52 -0300

    fix: correct daisyui import

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,15 @@
 import { sveltekit } from "@sveltejs/kit/vite";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
+import { resolve } from "path";
 
 export default defineConfig({
   plugins: [tailwindcss(), sveltekit()],
+  resolve: {
+    alias: {
+      daisyui: resolve("./node_modules/daisyui/index.js"),
+    },
+  },
   server: {
     port: 3000,
   },
